2add24d94839a50773eb1c91a745a6c16f26f034
Gabriel Ángel Canals Salleras <gabriel.canals.salleras@gmail.com>
2023-10-16 16:23:12 +0100

    Prepare l10n

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,29 @@
+import 'package:fitbro/services/auth/bloc/auth_bloc.dart';
+import 'package:fitbro/services/auth/firebase_auth_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 void main() {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(
+    MaterialApp(
+      supportedLocales: AppLocalizations.supportedLocales,
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      debugShowCheckedModeBanner: false,
+      title: 'FitBro',
+      theme: ThemeData(
+        primarySwatch: Colors.amber,
+      ),
+      home: BlocProvider<AuthBloc>(
+        create: (context) => AuthBloc(FirebaseAuthProvider()),
+        child: const MyApp(),
+      ),
+      routes: {
+        
+      },
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
