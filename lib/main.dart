be681a1599788d5f6a1df6af083f37a04d0a9c77
Gabriel Ángel Canals Salleras <gabriel.canals.salleras@gmail.com>
2023-10-18 23:36:16 +0100

    Main color setted by preferences

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
-import 'package:fitbro/constants/colors.dart';
 import 'package:fitbro/homepage.dart';
 import 'package:fitbro/services/auth/bloc/auth_bloc.dart';
 import 'package:fitbro/services/auth/firebase_auth_provider.dart';
+import 'package:fitbro/tools/settings/settings.dart';
+import 'package:fitbro/views/config/settings_menu_view.dart';
 import 'package:fitbro/views/oveview_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -9,8 +10,9 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 import 'constants/routes.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await preferences.settingsInit();
   runApp(
     MaterialApp(
       supportedLocales: AppLocalizations.supportedLocales,
@@ -18,7 +20,7 @@ void main() {
       debugShowCheckedModeBanner: false,
       title: 'FitBro',
       theme: ThemeData(
-        primarySwatch: mainColor,
+        primarySwatch: preferences.getMainColor(),
       ),
       home: BlocProvider<AuthBloc>(
         create: (context) => AuthBloc(FirebaseAuthProvider()),
@@ -26,6 +28,7 @@ void main() {
       ),
       routes: {
         overviewPageRoute: (context) => const OverviewView(),
+        configRoute: (context) => SettingsMenu(),
       },
     ),
   );
