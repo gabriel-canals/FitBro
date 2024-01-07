a3a6f03f247c07cc38cbe1dd47942f90cb576e75
Gabriel Ángel Canals Salleras <alu0101460468@ull.edu.es>
2024-01-07 19:14:43 +0000

    Added a Navigation Bar

--- lib/views/oveview_view.dart
+++ lib/views/oveview_view.dart
@@ -1,3 +1,6 @@
+import 'dart:math';
+
+import 'package:fitbro/constants/colors.dart';
 import 'package:fitbro/constants/routes.dart';
 import 'package:fitbro/extensions/buildcontext/loc.dart';
 import 'package:flutter/material.dart';
@@ -23,6 +26,96 @@ class _OverviewViewState extends State<OverviewView> {
               icon: const Icon(Icons.settings)),
         ],
       ),
+      body: const NavigationBarApp(
+      ),
+    );
+  }
+}
+
+class NavigationBarApp extends StatelessWidget {
+  const NavigationBarApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(useMaterial3: true),
+      home: const MainNavBar(),
+    );
+  }
+}
+
+class MainNavBar extends StatefulWidget {
+  const MainNavBar({super.key});
+
+  @override
+  State<MainNavBar> createState() => _MainNavBarState();
+}
+
+class _MainNavBarState extends State<MainNavBar> {
+  int currentPageIndex = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData theme = Theme.of(context);
+    return Scaffold(
+      extendBody: true,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.person),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: NavigationBar(
+        indicatorShape: const CircleBorder(),
+        height: 55,
+        onDestinationSelected: (int index) {
+          setState(() {
+            currentPageIndex = index;
+          });
+        },
+        indicatorColor: mainColor,
+        selectedIndex: currentPageIndex,
+        //!ERROR: Localizations are not supported for this class
+        destinations: <Widget>[
+          const NavigationDestination(
+            selectedIcon: Icon(Icons.fitness_center),
+            icon: Icon(Icons.fitness_center_outlined),
+            label: 'Entrenamiento',
+          ),
+           NavigationDestination(
+            selectedIcon: const Icon(Icons.restaurant_outlined),
+            icon: const Icon(Icons.restaurant),
+            label: "Alimenación",
+          ),
+        ],
+      ),
+      body: <Widget>[
+        
+        /// Entrenamiento
+        Card(
+          shadowColor: Colors.transparent,
+          margin: const EdgeInsets.all(6.0),
+          child: SizedBox.expand(
+            child: Center(
+              child: Text(
+                'Entrenamiento',
+                style: theme.textTheme.titleLarge,
+              ),
+            ),
+          ),
+        ),
+
+        /// Overview page
+        const Padding(
+          padding: EdgeInsets.all(6.0),
+          child: Text('Overview'),
+        ),
+
+        /// Dietas
+        const Padding(
+          padding: EdgeInsets.all(6.0),
+          child: Placeholder()
+        ),
+      ][currentPageIndex],
     );
   }
 }
